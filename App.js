f6b13ad87334327872e300e23361322aa809c536
Nadhif Mahardika <awandi8991@gmail.com>
2023-12-14 07:01:56 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, SafeAreaView, StyleSheet, Text, TextInput, View, Pressable } from 'react-native';
+import { Dialogflow_V2 } from 'react-native-dialogflow';
+
+import { dialogflowConfig } from './env';
 
 export default function App() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([
+    {
+      id: '2',
+      text: 'How can I help You ?',
+      userId: 2,
+    },
+    {
+      id: '1',
+      text: 'Hi, I am Dialog Flow Aggent',
+      userId: 2,
+    },
+  ]);
+
+  useEffect(() => {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id,
+    );
+  }, []);
+
+  const submitHandler = (text) => {
+    setMessages(prevMessages => {
+      return [
+        {
+          id: Math.random().toString(),
+          text: text,
+          userId: 1,
+        },
+        ...prevMessages
+      ];
+    });
+
+    setMessage('');
+
+    Dialogflow_V2.requestQuery(
+      text,
+      result => handleGoogleResponse(result),
+      error => console.log(error)
+    );
+  }
+
+  const handleGoogleResponse = (result) => {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    sendBotResponse(text);
+  }
+
+  const sendBotResponse = (text) => {
+    setMessages(prevMessages => {
+      return [
+        {
+          id: Math.random().toString(),
+          text: text,
+          userId: 2,
+        },
+        ...prevMessages
+      ];
+    });
+  }
+  
+  const MessageBubble = ({item}) => {
+    if (item.userId == 1) {
+      return (
+        <View
+          style={[styles.messageBubble, styles.myMessageBubble]}>
+          <Text style={styles.myMessageText}>{item.text}</Text>
+        </View>
+      );
+    }
+  
+    return (
+      <View style={styles.messageBubble}>
+        <Text style={styles.messageText}>{item.text}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.containerMessage}>
+        <FlatList
+          data={messages}
+          renderItem={MessageBubble}
+          keyExtractor={item => item.id}
+          inverted
+        />
+      </View>
+      <View style={styles.containerSubmitMessage}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setMessage}
+          value={message}
+          placeholder="Input message"
+        />
+        <Pressable style={styles.button} onPress={() => submitHandler(message)}>
+          <Text style={styles.buttonText}>Send</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +113,60 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginHorizontal: 16,
+  },
+  containerMessage: {
+    flex: 10,
+  },
+  containerSubmitMessage: {
+    flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    flex: 10,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 8,
+    fontSize: 16,
+  },
+  button: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    elevation: 3,
+    backgroundColor: '#3784FF',
+    marginLeft: 14,
+  },
+  buttonText: {
+    fontSize: 14,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  messageBubble: {
+    alignSelf: 'flex-start',
+    maxWidth: 280,
+    padding: 10,
+    borderRadius: 8,
+    marginVertical: 5,
+    marginHorizontal: 5,
+    backgroundColor: '#F1F0F0',
+  },
+  myMessageBubble: {
+    alignSelf: 'flex-end',
+    backgroundColor: '#3784FF',
+  },
+  messageText: {
+    fontSize: 16,
+  },
+  myMessageText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
---
+++ env.js
@@ -0,0 +1,13 @@
+export const dialogflowConfig = {
+    "type": "service_account",
+    "project_id": "chatbot-htey",
+    "private_key_id": "0c5d05c2ba5a0edd196657e20828747cb9bc9435",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDdzGVMjNjDICD4\nnvdRFoguDjZdZTsDz137kq5rpWRxFmNpbUBFIsbPPS6nYXaxXc1wj8uv6divdatw\nWqad2Z/GQMh41WKJUfiQbwsouOIELM62EOZGciuxzr49Bcow+ME6vZf/9aV7E2st\n1NXGg7zCget2w+/6Za8yJOasEMOH6bmwpXZY/0Ka4txm+H8tXQqr/7iRiS0MpNNA\nCD/pigsMb2yvwpBiAawy4jhST65dExDHjBpGfwqLKT0XMMvzLl6HqdTGeS35iFoO\noqtrM0YUg79CAO+HakLdF/k1rqZoMut7f+b/smoRMuBULHkRewx1RXZnX+1xNA2p\nTk0wdyRBAgMBAAECggEAOlnPHqQBVNhLhE0CAK2+Vut7RvfeXkK8VXaB96Tz90Ju\nFerdmTKsfhH5B6T+/muQpDfYXOfXD1YUK8NViTqbK6OYeh2dRkYv8AOeunBlWbbW\nDRUGF+tqbmVXLCOtTVPsNgFyDy83gVka/dw7/3ViAAQBB+ZFaKk+vaDypLrmTgdk\nF0b+VoydnYCQ6bzvrkT5Lyie3UIZHxT3NsL3ipilPSfSRJ22HHBLduKap2JDzOFe\nlyVTSCD53fOG6cNFa/H2C8RFS9phxoKEixfCu8tBwAa1Wf3dJsgRwHWNhyqDZSIl\nj+5fSXxjGCu1MwbO9AZ4r72DtYs1kdV8fbBAvBvanwKBgQD/tslArZIt/QjT8Q3x\n7C+4BW/LxKvmWqS/i4EUYqGXjTQBgdZ8hTFpEmDCKMrJ4/Ks/52YxLLDiXqj8iIN\nn2Ysfy55tPk4tFS4LT8uqDNhnlazJ22+pEY3bsilxxGD1Xjcejl0r24R5pH6Vso+\nmSEHyr/IxXM9Z00xPJQW5qHyYwKBgQDeC+YtpMoui/UQnXGM0EOQ0Rq1O9paWAVQ\nWv/7nKR+qmL5f1VgIGUmo3xDpHQW30cL9JhrOnrMsrcOXVX3UVV3Q/caxm8DxVl6\nlXWat27m7foRn88yVTEO70B8UeegRA353ESM2U2UHxg5tq2LbkViSU9disckn6v0\nYRNaQId+CwKBgQCW7DLr0YWtTuKe9G7ekh+35L2O7ozmcCk8RAHzHiNxZw2Ctjb5\nV0NKWGZzMdA+SCM6p8NEHxPDBKk1ifs/Pwz2xjmP65gMN6Ko8UuRJgTtYJ6bLnI/\nSoWGwgP6k/tqa9vhhOHUVJYKSqNttTu4pngwOSHoRWE2pHmSzcY5nfWHMQKBgCMv\nfYUEFOm1UdsVJ9YpdltvVy0a0+n3AriOAvxLASH1PAPPMqct2vl5dIp/d8ncDbUO\nSEzNkoH3W9yR27p6D/H7Oh/rC9JsJlavclhW8uLzGOpMCZgvObvlG7HXhL23qwNJ\n7S7ojEuJddxHX0615ZbCiSoIgLAEAUqTS27ExPOrAoGBAJVx0zCoTjt/oauk2+kj\nrrK47WDr0PheRPdZkMnPPCRtDq/lv+CTFRGkpmvi9JNK9TjtddDk2bEdUxCyH+uu\nIXE6edVvwfETlm3AAVZll8L28VJfoZQFJl6U+Qg9ly4VSsR7ZYE3eMbpBDYivBOu\nY+kDZVxVzAPuBfrRfRnnDnmc\n-----END PRIVATE KEY-----\n",
+    "client_email": "chatbot@chatbot-htey.iam.gserviceaccount.com",
+    "client_id": "100022133399568607103",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/chatbot%40chatbot-htey.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  };  
